006fcc5c983662a819a353e2256e5a3651915b0f
Rukin Danil <Rukin2018D@yandex.ru>
2022-01-11 15:47:30 +0700

    Адаптер

---
+++ Infrastructure/ExistingServices/ExistingEncryptor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ExistingServices
+{
+	public class ExistingEncryptor
+	{
+		public int GetHashCodeFromData(string data) => data.GetHashCode();
+
+		public string GetTextFromHashCode(int code) => code.ToString();
+	}
+}
---
+++ Infrastructure/Services/EncryptorAdapter.cs
@@ -0,0 +1,48 @@
+﻿using Infrastructure.ExistingServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemCore.Services;
+
+namespace Infrastructure.Services
+{
+	public class EncryptorAdapter : IEncryptor<string, string>
+	{
+		private ExistingEncryptor _adaptableEncryptor;
+		public EncryptorAdapter(ExistingEncryptor existingEncryptor)
+		{
+			_adaptableEncryptor = existingEncryptor;
+		}
+		public string DecryptData(string data)
+		{
+			int num = StrToInt(data);			
+			return _adaptableEncryptor.GetTextFromHashCode(num);
+		}
+
+		public string EncryptData(string data)
+		{
+			int hashCode = _adaptableEncryptor.GetHashCodeFromData(data);
+			return hashCode.ToString();
+		}
+
+		private int StrToInt(string str)
+		{
+			StringBuilder builder = new StringBuilder(str.Length);
+			for (int i = str.Length - 1; i >= 0; i--)
+			{
+				builder.Append(str[i]);
+			}
+			string reverced = builder.ToString();
+			int currentNum;
+			int sum = 0;
+			for (int i = 0; i < reverced.Length; i++)
+			{
+				currentNum = Convert.ToInt32(reverced[i]);
+				sum += currentNum + (int)Math.Pow(10, i);
+			}
+			return sum;
+		}
+	}
+}
